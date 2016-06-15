4a85ffb6cd56534a2eefca5ce074c2f837017f59
zhufengnodejs <zhufengnodejs@126.com>
2016-06-15 11:40:49 +0800

    使用配置文件来进行配置输入输出路径

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 module.exports = {
     //指定入口文件 使用绝对路径
-    entry:path.resolve(__dirname,'app/index.js'),
+    entry:path.resolve(__dirname,'src/index.js'),
     output:{//配置打包结果
         path:path.resolve(__dirname,'build'),//指定输出文件的目录
         filename:'bundle.js' // 指定输出文件的名称
