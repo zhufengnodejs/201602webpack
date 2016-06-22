03919be1cfc1c9dfa278ab5cc595c3a39b989535
zhufengnodejs <zhufengnodejs@126.com>
2016-06-22 17:04:37 +0800

    1.实现最基础留言版

---
+++ comment/components/CommentBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class CommentBox extends React.Component{
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12">
+                         欢迎留言
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ comment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {CommentBox} from './components';
+
+ReactDOM.render(
+    <CommentBox/>,
+    document.querySelector('#app')
+);
